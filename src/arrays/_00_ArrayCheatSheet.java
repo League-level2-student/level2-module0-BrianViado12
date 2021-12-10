0ce3028d01598b5bb413b9f5162df3c58de60988
Daddy <Daddy@vivs-macpro-13>
2021-12-09 17:25:08 -0800

    did this 12/9/21

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,23 +1,46 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] string = {"A", "B", "C", "E", "F"};
 		//2. print the third element in the array
-		
+		System.out.println(string[2]);
 		//3. set the third element to a different value
-		
+		string[2] = "G";
 		//4. print the third element again
-		
+		System.out.println(string[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		for (int i = 0; i < string.length; i++) {
+			System.out.println(string[i]);
+		}
 		
 		//6. make an array of 50 integers
-		
+		int[] num = new int[50];
 		//7. use a for loop to make every value of the integer array a random number
-		
+		for (int i = 0; i < num.length; i++) {
+			num[i] = new Random().nextInt();
+			
+		}
+		int low = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if(num[i]<=low) {
+				low = num[i];
+			
+			}
+			
+		}
+		System.out.println(low);
+		int high = num[0];
+		for (int i = 0; i < num.length; i++) {
+			if(num[i]>=high) {
+				high = num[i];
+			}
+		}
+		System.out.println(high);
 		//8. without printing the entire array, print only the smallest number in the array
 		
 		//9 print the entire array to see if step 8 was correct
